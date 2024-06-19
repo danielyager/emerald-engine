fe47342882eaf81e710c2d8687cfbcb193494cba
Daniel <danielyager473@gmail.com>
2024-06-18 19:20:54 -0700

    started adding glfw and opengl with test code to create a basic window

--- Emerald/src/EmeraldCore/Application.cpp
+++ Emerald/src/EmeraldCore/Application.cpp
@@ -2,8 +2,13 @@
 #include "Application.h"
 #include "Events/ApplicationEvent.h"
 #include "Logger.h"
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
 namespace Emerald {
+
+	void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+	void processInput(GLFWwindow* window);
 
 	Application::Application() {
 	}
@@ -12,9 +17,60 @@ namespace Emerald {
 	}
 
 	void Application::Run() {
+		glfwInit();
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+		//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+		GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+		if (!window) {
+			std::cout << "Failed to create GLFW window" << std::endl;
+			glfwTerminate();
+			return;
+		}
+		glfwMakeContextCurrent(window);
+		glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+		if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+			std::cout << "Failed to initialize GLAD." << std::endl;
+			return;
+		}
+
+		glViewport(0, 0, 800, 600);
+
+		//glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+		while (!glfwWindowShouldClose(window)) {
+			// input
+			processInput(window);
+
+			// Rendering commands
+			glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+			glClear(GL_COLOR_BUFFER_BIT);
+
+			// Check and call events and swap the buffers
+			glfwSwapBuffers(window);
+			glfwPollEvents();
+		}
+
+		glfwTerminate();
+
+
 		WindowResizeEvent e(1280, 720);
 		LOG_TRACE(0, e.ToString());
-		while (true);
+		//while (true);
+	}
+
+	void processInput(GLFWwindow* window) {
+		if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+			glfwSetWindowShouldClose(window, true);
+	}
+
+	void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+		// make sure the viewport matches the new window dimensions; note that width and 
+		// height will be significantly larger than specified on retina displays.
+		glViewport(0, 0, width, height);
 	}
 
 }
